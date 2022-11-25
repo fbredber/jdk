15f682d8936d396743687a1f80335eb79ab27735
Fredrik Bredberg <fredrik.bredberg@oracle.com>
2022-11-25 14:52:08 +0100

    frbr

--- src/hotspot/share/runtime/frame.cpp
+++ src/hotspot/share/runtime/frame.cpp
@@ -392,8 +392,8 @@ frame frame::real_sender(RegisterMap* map) const {
 
 void frame::interpreter_frame_set_locals(intptr_t* locs)  {
   assert(is_interpreted_frame(), "Not an interpreted frame");
-  // *addr_at(interpreter_frame_locals_offset) = (intptr_t)(locs - fp()); @frbr@
-  *interpreter_frame_locals_addr() = locs;
+  // *addr_at(interpreter_frame_locals_offset) = (intptr_t)(locs - fp()); // @frbr@
+  *interpreter_frame_locals_addr() = locs; // @frbr@
 }
 
 Method* frame::interpreter_frame_method() const {
@@ -465,8 +465,8 @@ BasicObjectLock* frame::previous_monitor_in_interpreter_frame(BasicObjectLock* c
 
 intptr_t* frame::interpreter_frame_local_at(int index) const {
   const int n = Interpreter::local_offset_in_bytes(index)/wordSize;
-  // intptr_t* first = interpreter_frame_locals_addr(); @frbr@
-  intptr_t* first = _on_heap ? fp() + (intptr_t)*interpreter_frame_locals_addr()
+  // intptr_t* first = interpreter_frame_locals_addr(); // @frbr@
+  intptr_t* first = _on_heap ? fp() + (intptr_t)*interpreter_frame_locals_addr() // @frbr@
                              : *interpreter_frame_locals_addr();
   return &(first[n]);
 }
